c2e1097cb9c645317d028e2920a99404b560f1ce
Jonny <jonny.howle@gmail.com>
2022-12-28 11:48:51 -0700

    added basic structure form structure for adding needs

--- src/components/AddNeed.js
+++ src/components/AddNeed.js
@@ -2,9 +2,41 @@ import React, {useState} from "react";
 
 function AddNeed() {
 
+    const [formData, setFormData] = useState({
+        neighborName: "",
+        amount: 0,
+        category: null,
+        description: "",
+    })
+
+    function handleChange(e) {
+        const name = e.target.name
+        let value = e.target.value
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(formData)
+    }
+
     return (
         <div>
-            <h1>Add Need Form Goes Here</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Neighbor Name
+                    <input
+                    type="text"
+                    name="neighborName"
+                    onChange={handleChange}
+                    value={formData.neighborName}
+                    />
+                </label>
+
+            </form>
         </div>
     )
 }
